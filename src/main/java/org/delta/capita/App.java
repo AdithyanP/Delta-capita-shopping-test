10dd8a1adc692228b8b0856df8a119d290eb6526
Adithyan <adithyanp@crayondata.com>
2024-03-31 15:51:17 +0530

    feat : add project structure

---
+++ src/main/java/org/delta/capita/App.java
@@ -0,0 +1,13 @@
+package org.delta.capita;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
