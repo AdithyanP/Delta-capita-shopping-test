63edb3f8568a7bc36864ad7418ce1d7971e6b716
Adithyan <adithyanp@crayondata.com>
2024-04-02 00:27:25 +0530

    feat : add total calculation of basket logic

---
+++ src/test/java/org/delta/capita/service/BasketCalculationTest.java
@@ -0,0 +1,12 @@
+package org.delta.capita.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasketCalculationTest {
+
+    @Test
+    void calculate_total_cost_item() {
+    }
+}
