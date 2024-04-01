9b80bb8645569bfbb8875974c380197079cedcab
Adithyan <adithyanp@crayondata.com>
2024-04-02 00:57:35 +0530

    feat : call basket application from main.

--- src/main/java/org/delta/capita/App.java
+++ src/main/java/org/delta/capita/App.java
@@ -1,16 +1,23 @@
 package org.delta.capita;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.delta.capita.domain.BasketEnum;
+import org.delta.capita.service.BasketCalculation;
+
+import java.util.List;
 
 /**
  * Hello world!
  *
  */
-@SpringBootApplication
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Basket calculation application has started!!!" );
+        BasketCalculation basketCalculation = new BasketCalculation();
+        var listOfItems = List.of(BasketEnum.Apple,BasketEnum.Bananas,BasketEnum.Limes,BasketEnum.Limes,
+                BasketEnum.Limes,BasketEnum.Limes,BasketEnum.Limes,BasketEnum.Limes,BasketEnum.Limes);
+       var totalCostOfItems =  basketCalculation.calculate_total_cost_item(listOfItems);
+       System.out.println("totalCostOfItems: "+totalCostOfItems);
     }
 }
